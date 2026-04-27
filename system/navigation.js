56382019a08332fccbbf9d9f459f48ae98fd08d5
monsterfurious <guidotambarogp@gmail.com>
2026-04-28 00:29:40 +0200

    [MAJOR CHANGE] normalize navbar and css

--- system/navigation.js
+++ system/navigation.js
@@ -1,8 +1,9 @@
-const navTemplate = `
+function getNavTemplate(base) {
+    return `
 <nav class="global-nav">
     <button class="menu-toggle" id="menu-btn">&#9776;</button>
-    <a href="../../index.html" class="nav-logo-container">
-        <img src="../../img/logo.png" alt="SOULSPECTRE LOGO">
+    <a href="${base}index.html" class="nav-logo-container">
+        <img src="${base}img/logo.png" alt="SOULSPECTRE LOGO">
     </a>
 </nav>
 
@@ -11,21 +12,21 @@ const navTemplate = `
 <aside class="sidebar" id="sidebar">
     <button class="close-sidebar" id="close-btn">&times;</button>
     
-    <a href="../../index.html" class="direct-link">Main Hub</a>
+    <a href="${base}index.html" class="direct-link">Main Hub</a>
 
     <button class="accordion-btn">Gameplay <span class="chevron">▼</span></button>
     <div class="accordion-content">
-        <a href="../../gameplay/basics.html" class="sub-link">Core Rules</a>
-        <a href="../../gameplay/cities.html" class="sub-link">Cities Mechanics</a>
-        <a href="../../gameplay/mapresources.html" class="sub-link">Map And Resources</a>
-        <a href="../../gameplay/officers.html" class="sub-link">Officers and Parties</a>
-        <a href="../../gameplay/progression.html" class="sub-link">Characters Progression</a>
+        <a href="${base}gameplay/basics.html" class="sub-link">Core Rules</a>
+        <a href="${base}gameplay/cities.html" class="sub-link">Cities Mechanics</a>
+        <a href="${base}gameplay/mapresources.html" class="sub-link">Map And Resources</a>
+        <a href="${base}gameplay/officers.html" class="sub-link">Officers and Parties</a>
+        <a href="${base}gameplay/progression.html" class="sub-link">Characters Progression</a>
     </div>
 
     <button class="accordion-btn">Factions <span class="chevron">▼</span></button>
     <div class="accordion-content">
-        <a href="../../factions/valkyrion.html" class="sub-link" style="color: #5c9ce6;">Valkyrion Empire</a>
-        <a href="../../factions/forgotten.html" class="sub-link" style="color: #5cb88a;">The Forgotten</a>
+        <a href="${base}factions/valkyrion.html" class="sub-link" style="color: #5c9ce6;">Valkyrion Empire</a>
+        <a href="${base}factions/forgotten.html" class="sub-link" style="color: #5cb88a;">The Forgotten</a>
         <a href="#" class="sub-link locked">Kharos Dominion</a>
         <a href="#" class="sub-link locked">Devil Syndicate</a>
         <a href="#" class="sub-link locked">Primal Coalition</a>
@@ -35,17 +36,19 @@ const navTemplate = `
 
     <button class="accordion-btn">Campaigns <span class="chevron">▼</span></button>
     <div class="accordion-content">
-        <a href="../../campaigns/story_mode.html" class="sub-link">Story Mode</a>
-        <a href="../../campaigns/arcade_mode.html" class="sub-link">Arcade Mode</a>
+        <a href="${base}campaigns/story_mode.html" class="sub-link">Story Mode</a>
+        <a href="${base}campaigns/arcade_mode.html" class="sub-link">Arcade Mode</a>
     </div>
 
-    <a href="../../about/credits.html" class="direct-link">The Architects</a>
+    <a href="${base}about/credits.html" class="direct-link">The Architects</a>
 </aside>
 `;
+}
 
 class GlobalNav extends HTMLElement {
     connectedCallback() {
-        this.innerHTML = navTemplate;
+        const base = this.getAttribute('base') || '../../';
+        this.innerHTML = getNavTemplate(base);
         this.initScripts();
     }
 
